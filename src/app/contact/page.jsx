3c91c078ed8a9fb2eea128fc814552c826aaf3a1
devgabrielcd <devgabrielcd@gmail.com>
2026-06-17 16:10:26 -0300

    Update contact map address

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -48,7 +48,7 @@ export default function ContactPage() {
                     </div>
                     <div className="info-content">
                       <h3>Our Address</h3>
-                      <p>1000 NW 65th St, Fort Lauderdale, FL 33309</p>
+                      <p>1000 NW 65th St #103, Fort Lauderdale, FL 33309</p>
                     </div>
                   </div>
                   <div
@@ -104,7 +104,7 @@ export default function ContactPage() {
           >
             <div className="map-overlay" />
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d48389.78314118045!2d-74.006138!3d40.710059!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25a22a3bda30d%3A0xb89d1fe6bc499443!2sDowntown%20Conference%20Center!5e0!3m2!1sen!2sus!4v1676961268712!5m2!1sen!2sus"
+              src="https://www.google.com/maps?q=1000%20NW%2065th%20St%20%23103%2C%20Fort%20Lauderdale%2C%20FL%2033309%2C%20USA&output=embed"
               width="100%"
               height={500}
               style={{ border: 0 }}
